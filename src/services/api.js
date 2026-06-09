68a164339df3c148e1581f14d8fddf3a16fd0628
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-06-09 13:14:37 +0530

    Update frontend application

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Set base URL for API calls
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL ;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
